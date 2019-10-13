622f27d9e7e250ff333f620586ef7ec797742504
Goose <crusty512@yahoo.com>
2019-10-13 02:16:47 -0500

    progression projection view

--- src/components/classes/Projection.js
+++ src/components/classes/Projection.js
@@ -1,22 +1,121 @@
-import React from 'react'
+import React, { Component } from "react";
+import { connect } from "react-redux";
 import { Redirect } from "react-router-dom";
+import CancelPresentationIcon from "@material-ui/icons/CancelPresentation";
+import AspectRatioIcon from "@material-ui/icons/AspectRatio";
+import { Card, Button } from "react-materialize";
+import Fullscreen from "react-full-screen";
 
+class Projection extends Component {
+  state = {
+    isFull: false,
+    slideNumber: 1
+  };
 
-const Projection = ({  }) => {
-    const { session, auth, authError } = this.props;
-    console.log(authError)
+  goFull = () => {
+    this.setState({ isFull: !this.state.isFull });
+  };
+
+  render() {
+    const { auth, authError } = this.props;
+    console.log(authError);
 
     if (!auth.uid) {
-        return <Redirect to="/signin" />;
+      return <Redirect to="/signin" />;
     }
+
     return (
-        <div className="card z-depth-0 classs-summary">
-            <div className="card-content grey-text text-darken-3">
-                <span className="card-title"> sessions.currentSliceID - slices.Question
-                </span>
-            </div>
-        </div>
-    )
+      <div style={{ margin: "1em", padding: "0" }}>
+        <Fullscreen
+          enabled={this.state.isFull}
+          onChange={isFull => this.setState({ isFull })}
+        >
+          <Card
+            className="white full-screenable-node"
+            textClassName="black-text"
+            title="Slide 1"
+            style={{ height: "100%", position: "relative" }}
+            onClick={this.goFull}
+          >
+            This a bunch of drunkenly typed text that should show up on a
+            fullscreen projetion view fingers crossed. This may cover the entire
+            monitor.
+          </Card>
+          {this.state.isFull ? (
+            <CancelPresentationIcon
+              style={{
+                fontSize: "xxx-large",
+                color: "red",
+                position: "absolute",
+                top: "1%",
+                right: "1%"
+              }}
+              onClick={this.goFull}
+            />
+          ) : (
+            <AspectRatioIcon
+              style={{
+                fontSize: "xxx-large",
+                color: "red",
+                position: "absolute",
+                top: "1%",
+                right: "1%"
+              }}
+              onClick={this.goFull}
+            />
+          )}
+          <div
+            style={{
+              fontSize: "xxx-large",
+              color: "black",
+              position: "absolute",
+              bottom: "2%",
+              left: "2%",
+              display: "inline"
+            }}
+          >
+            <img
+              src="https://i.imgur.com/cwulpWU.png"
+              alt=""
+              style={{ height: "1em" }}
+            />
+            <h3
+              style={{
+                cursor: "default",
+                float: "right",
+                bottom: "-6%",
+                position: "absolute",
+                left: "118%"
+              }}
+            >
+              piEnabler
+            </h3>
+          </div>
+
+          <div
+            style={{
+              fontSize: "xxx-large",
+              color: "black",
+              position: "absolute",
+              bottom: "2%",
+              right: "2%"
+            }}
+          >
+            <h1>{this.state.slideNumber}</h1>
+          </div>
+        </Fullscreen>
+      </div>
+    );
+  }
 }
 
-export default Projection
+const mapStateToProps = state => ({
+  auth: state.firebase.auth
+});
+
+const mapDispatchToProps = dispatch => ({});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Projection);
