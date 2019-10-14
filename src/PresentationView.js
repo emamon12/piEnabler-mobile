a9c5488630906af88683b933b6876609bce083ee
Ryan Cassens <44488444+rcassens@users.noreply.github.com>
2019-10-14 17:32:39 -0500

    Create PresentationView.js

---
+++ src/PresentationView.js
@@ -0,0 +1,149 @@
+import React from 'react';
+import {Button} from 'react-native';
+
+class PresentationView {
+    render() {
+        return (
+            <div className="wrapper">
+
+            <div className="question-wrapper">
+                <div className="student-question-screen">
+
+                </div>
+                <Button onPress = {}
+                            title = "Deploy Questions"
+                            color = "red"/>
+    
+                    </div>
+
+    
+            <div className="top-container">
+                <div className="left">
+                    <div className="top-left">
+                        <Button onPress = {}
+                            title = "Current Slice"
+                            color = "red"/>
+    
+                    </div>
+    
+                    <div className="bottom-left">
+                    <Button onPress = {}
+                            title = "Next Slice"
+                            color = "red"/>
+    
+                    </div>
+                </div>
+    
+                <div className="right">
+                    <div className="histogram">
+                    <Button onPress = {}
+                            title = "Show Histogram"
+                            color = "red"/>    
+                    </div>
+                    
+                    <div className="bottom-right">
+
+    
+                    </div>
+                </div>
+            </div>
+    
+            <div className="ribbon">
+             <div className="lecture">
+             <Button onPress = {}
+                            title = "Lecture Slice"
+                            color = "red"/>
+                </div>
+                <div className="similar-slclassNamee"> 
+                <Button onPress = {}
+                            title = "Similar Slice"
+                            color = "red"/>
+                </div>
+                <div className="harder-slclassNamee">
+                <Button onPress = {}
+                            title = "Harder Slice"
+                            color = "red"/>
+                </div>
+                <div className="new-slclassNamee">
+                <Button onPress = {}
+                            title = "New Topic"
+                            color = "red"/>
+                </div>
+                <div className="histogram-div">
+                <Button onPress = {}
+                            title = "Show Histogram"
+                            color = "red"/>
+                </div>-->
+            </div>
+    
+        </div>
+        );
+
+    }
+
+}
+
+export default PresentationView
+
+const sytle = StyleSheet.create({
+   
+    /*h1: {
+        color: black,
+        alignItems: 'center'
+    }
+    
+    wrapper: {
+        alignItems: 'center',
+        margin: auto 0
+    }
+    
+    @media only screen and (max-width:800px) {
+    /* For tablets: */
+    /*.wrapper {
+    width: 80%;
+    padding: 0;
+    }
+    img {
+    width: 45%;
+    height: 45%;
+    }
+    }
+    @media only screen and (max-width:500px) {
+    /* For mobile phones: */
+    /*.wrapper {
+    width: 70%;
+    }
+    
+    img {
+    width: 35%;
+    height: 35%;
+    
+    }
+    
+    #wrapper {
+        width: 100%;
+        margin: 0 auto;
+    }
+    
+    #top-container {
+        width:10px;
+        display:block;
+        vertical-align: middle;
+        float:right;
+        padding-left: 100px;
+    }
+    
+    #left {
+    
+    left:0;
+    }
+    
+    #right {
+    right:0;
+    }
+    
+    #top-left {
+    position: absolute;
+    margin-top: -10px;
+*/
+})
