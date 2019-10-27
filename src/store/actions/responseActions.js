ab60ca5ace9fc0f967a19038188473f9bfacb15c
Ryan Cassens <44488444+rcassens@users.noreply.github.com>
2019-10-26 19:18:47 -0500

    Update responseActions.js

--- src/store/actions/responseActions.js
+++ src/store/actions/responseActions.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 const db = firestore.database();
 var classesReference = firebase.database().ref("Classes/");
 var studentRef = firebase.database().ref("users/");
@@ -89,9 +90,68 @@ function getInfoForHistogram() {
   Console.log("B: " + bCount);
   Console.log("C: " + cCount);
   Console.log("D: " + dCount);
+=======
+export const getResponses = fbase => (dispatch, setStatem {getFirestore}) => {
+	const fireStore = getFirestore();
+	const responses = fbase.sliceId;
+	const sess = fbase.sessionId;
+	const place;
+	var aCount, bCount, cCount, dCount;
+	
+	place = fireStore.collection('sliceResponses').doc('${responses}${sess}'};
+	
+	aCount = fbase.numberOfA;
+	bCount = fbase.numberOfB;
+	cCount = fbase.numberOfC;
+	dCount = fbase.numberOfD;
+	
+	let observer = place.onSnapshot(docSnapshot => {
+		console.log("A: " + aCount + '\n');
+		console.log("B: " + bCount + '\n');
+		console.log("C: " + cCount + '\n');
+		console.log("D: " + dCount + '\n');
+	}, err => {
+  console.log('Encountered error: ${err}');
+});
+	
 }
 
-function getOptions(data) {
+export const updateResponses = fbase => (dispatch, getState, {getFirestore}) => {
+	const fireStore = getFirestore();
+	const sess = fbase.session;
+	const sessId = fbase.sessionId;
+	const response = fbase.uAnswer;
+	const numA = fbase.numberOfA;
+	const numB = fbase.numberOfB;
+	const numC = fbase.numberOfC;
+	const numD = fbase.numberOfD;
+	const totalResp = fbase.totalResponses;
+	
+	if(response == 'A') {
+			numA++;
+	}
+	else if(response == 'B') {
+			numB++;
+	}
+	else if(response == 'C') {
+			numC++;
+	} else {
+			numD++;
+	}
+	
+	totalResp++;
+	
+fireStore.collection('sliceResponses').doc('${sessId}${sess.currentSliceId}'}.update({
+	numberOfA: numA,
+	numberOfB: numB,
+	numberofC: numC,
+	numberOfD: numD,
+	totalResponses: totalResp
+});
+>>>>>>> Stashed changes
+}
+
+/*function getOptions(data) {
   sliceInfo = data.val();
 
   isLecture = sliceInfo.Lecture;
@@ -114,4 +174,30 @@ function getOptions(data) {
   document.getElementById("answer2").innerHTML = answer2;
   document.getElementById("answer3").innerHTML = answer3;
   document.getElementById("answer4").innerHTML = answer4;
+}*/
+
+export const LoadQuestions = classs => (dispatch, getState, {getFirestore}) => {
+	const fireStore = getFirestore();
+	const collection = classs.classId;
+	const sliceToLoad = classs.sliceToLoad;
+	const var answer1, answer2, answer3, answer4;
+	
+	if(classs.isLecture == false) {
+		answer1 = classs.answer1;
+		answer2 = classs.answer2;
+		answer3 = classs.answer3;
+		answer4 = classs.answer4;
+	} else {
+	    answer1 = "";
+		answer2 = "";
+		answer3 = "";
+		answer4 = "";
+		correctAnswer = "";
+	}
+	
+  document.getElementById("answer1").innerHTML = answer1;
+  document.getElementById("answer2").innerHTML = answer2;
+  document.getElementById("answer3").innerHTML = answer3;
+  document.getElementById("answer4").innerHTML = answer4;
+	
 }
