4622844ada58f91f4c552c00922a0caa4c2260fb
Josh Jost <jjost@siue.edu>
2019-10-28 15:31:22 -0500

    Update responseActions.js

--- src/store/actions/responseActions.js
+++ src/store/actions/responseActions.js
@@ -1,104 +1,11 @@
-<<<<<<< Updated upstream
-const db = firestore.database();
-var classesReference = firebase.database().ref("Classes/");
-var studentRef = firebase.database().ref("users/");
-var answerRef = firebase.database().ref("responses/");
-var sliceRef = firebase.database().ref("slices/");
-var answer1, answer2, answer3, answer4, correctAnswer, isLecture;
-var sliceInfo;
-
-function sendResponseToDB(studentId, response, timesPolled, session, slice) {
-  //Check both methods
-  var studentRef = db.ref("piesiue/responses/" + studentId);
-  var docName = "test" + session;
-
-  let data = {
-    currSession: session,
-    currSlice: slice,
-    response: response,
-    studentId: studentId,
-    timesPolled: timesPolled
-  };
-
-  let setDoc = db
-    .collection("piesiue/responses")
-    .doc(docName)
-    .set(data);
-
-  //timesPolled++;
-  //newResponse = studentRef.response + "," + newResponse;
-
-  //firebase.database().ref(studentRef).set({
-  //	response: newResponse,
-  //	timesPolled: timesPolled,
-  //});
-}
-
-
-
-function pollSliceInfo(response, sliceId, sessionId, studentId) {
-  var sliceRef = db.ref("piesiue/slices/" + sliceId);
-  var responseRef = db.ref("piesiue/sliceResponses/");
-  var a1, a2, a3, a4, numA, numB, numC, numD, totalResponses;
-  var docId = sliceId + sessionId;
-
-  a1 = sliceRef.answer1;
-  a2 = sliceRef.answer2;
-  a3 = sliceRef.answer3;
-  a4 = sliceRef.answer4;
-  numA = sliceRef.numA;
-  numB = sliceRef.numB;
-  numC = sliceRef.numC;
-  numD = sliceRef.numD;
-  totalResponses = sliceRef.totalResponces;
-
-  if (response == a1) {
-    numA++;
-  } else if (response == a2) {
-    numB++;
-  } else if (response == a3) {
-    numC++;
-  } else {
-    numD++;
-  }
-
-  totalResponses++;
-
-  let data = {
-    numberOfA: numA,
-    numberOfB: numB,
-    numberOfC: numC,
-    numberOfD: numD,
-    totalResponces: totalResponses
-  };
-
-  sliceRef.update(data);
-  //db.collection('piesiue/sliceResponses/9MqKqnynbnOqzkYp38sn').set(data);
-}
-
-function getInfoForHistogram() {
-  // var sessionRef = db.ref('piesiue/session');
-  var sliceRef = db.ref("piesiue/slices");
-  var aCount, bCount, cCount, dCount;
-
-  aCount = sliceRef.numberOfA;
-  bCount = sliceRef.numberOfB;
-  cCount = sliceRef.numberOfC;
-  dCount = sliceRef.numberOfD;
-
-  Console.log("A: " + aCount);
-  Console.log("B: " + bCount);
-  Console.log("C: " + cCount);
-  Console.log("D: " + dCount);
-=======
-export const getResponses = fbase => (dispatch, setStatem {getFirestore}) => {
+export const getResponses = fbase => (dispatch, setState, {getFirestore}) => {
 	const fireStore = getFirestore();
 	const responses = fbase.sliceId;
 	const sess = fbase.sessionId;
 	const place;
 	var aCount, bCount, cCount, dCount;
 	
-	place = fireStore.collection('sliceResponses').doc('${responses}${sess}'};
+	place = fireStore.collection('sliceResponses').doc('${responses}${sess}');
 	
 	aCount = fbase.numberOfA;
 	bCount = fbase.numberOfB;
@@ -113,8 +20,8 @@ export const getResponses = fbase => (dispatch, setStatem {getFirestore}) => {
 	}, err => {
   console.log('Encountered error: ${err}');
 });
-	
 }
+
 
 export const updateResponses = fbase => (dispatch, getState, {getFirestore}) => {
 	const fireStore = getFirestore();
@@ -141,14 +48,13 @@ export const updateResponses = fbase => (dispatch, getState, {getFirestore}) => 
 	
 	totalResp++;
 	
-fireStore.collection('sliceResponses').doc('${sessId}${sess.currentSliceId}'}.update({
+fireStore.collection('sliceResponses').doc('${sessId}${sess.currentSliceId}').update({
 	numberOfA: numA,
 	numberOfB: numB,
 	numberofC: numC,
 	numberOfD: numD,
 	totalResponses: totalResp
 });
->>>>>>> Stashed changes
 }
 
 /*function getOptions(data) {
@@ -180,24 +86,50 @@ export const LoadQuestions = classs => (dispatch, getState, {getFirestore}) => {
 	const fireStore = getFirestore();
 	const collection = classs.classId;
 	const sliceToLoad = classs.sliceToLoad;
-	const var answer1, answer2, answer3, answer4;
+  var answer1, answer2, answer3, answer4, correctAnswer;
+  var show1, show2, show3, show4;
+
+  show1 =  document.getElementById("answer1");
+  show2 =  document.getElementById("answer2");
+  show3 =  document.getElementById("answer3");
+  show4 =  document.getElementById("answer4");
+
+  correctAnswer = classs.CorrectAnswer;
 	
 	if(classs.isLecture == false) {
-		answer1 = classs.answer1;
-		answer2 = classs.answer2;
-		answer3 = classs.answer3;
-		answer4 = classs.answer4;
+		answer1 = classs.Answer1;
+		answer2 = classs.Answer2;
+		answer3 = classs.Answer3;
+		answer4 = classs.Answer4;
 	} else {
-	    answer1 = "";
+	  answer1 = "";
 		answer2 = "";
 		answer3 = "";
 		answer4 = "";
 		correctAnswer = "";
-	}
-	
-  document.getElementById("answer1").innerHTML = answer1;
-  document.getElementById("answer2").innerHTML = answer2;
-  document.getElementById("answer3").innerHTML = answer3;
-  document.getElementById("answer4").innerHTML = answer4;
-	
+  }
+  
+  if(correctAnswer == answer1) {
+    show1.innerHTML = <b>answer1</b>;
+    show2.innerHTML = answer2;
+    show3.innerHTML = answer3;
+    show4.innerHTML = answer4;
+  } 
+  else if(correctAnswer == answer2) {
+    show1.innerHTML = answer1;
+    show2.innerHTML = <b>answer2</b>;
+    show3.innerHTML = answer3;
+    show4.innerHTML = answer4;
+  }
+  else if(correctAnswer == answer3) {
+    show1.innerHTML = answer1;
+    show2.innerHTML = answer2;
+    show3.innerHTML = <b>answer3</b>;
+    show4.innerHTML = answer4;
+  } else {
+    show1.innerHTML = answer1;
+    show2.innerHTML = answer2;
+    show3.innerHTML = answer3;
+    show4.innerHTML = <b>answer4</b>;
+  }
 }
